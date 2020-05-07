8e1adc516250c422be7c4fbab6049442e1c8a88f
Carmen Johnson <carmenjohnson512@gmail.com>
2020-05-07 01:57:37 -0500

    added line break on chkbx elmts, added bold styling to p elements, created chkbx variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,23 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var alphNumChar = "abcdegfhijklmnopqrstuvwxyz0123456789".split("");
-console.log("alphNumChar is working!", alphNumChar)
+let generateBtn = document.querySelector("#generate");
+let alphNumChar = "abcdegfhijklmnopqrstuvwxyz0123456789".split("");
+// console.log("alphNumChar is working!", alphNumChar)
 let specChar = " \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\^\_\`\{\|\}\~".split("");
-console.log("specChar is working!!", specChar)
+// console.log("specChar is working!!", specChar)
 let alphNumCharUpper = function () {alphNumChar.toUpperCase()};
-console.log(alphNumCharUpper)
+// console.log(alphNumCharUpper)
 let alphNumCharLower = function () {alphNumChar.toLowerCase()};
-console.log(alphNumCharLower)
+// console.log(alphNumCharLower)
+let alphNumCharChkBx = document.querySelector("#alphNumChar");
+// console.log("AlphNum chk box slector is working!", alphNumCharChkBx)
+let specCharChkBx = document.querySelector("#specChar");
+// console.log("SpecChar chk box slector is working!", specCharChkBx)
+let alphNumCharUpperChkBx = document.querySelector("#alphNumCharUpper");
+// console.log("Alph Num Upper is working!", alphNumCharUpperChkBx)
+let alphNumCharLowerChkBx = document.querySelector("#alphNumCharLower");
+// console.log("Alph Num Lower is working!", alphNumCharLowerChkBx)
+
+
 
 // declare function to select Alpha Numeric Character checkbox option
 function includeAlphNumChar() {
@@ -18,10 +28,13 @@ function includeAlphNumChar() {
 function includeSpecChar() {
 
 }
-// declare function to select Special Character checkbox option
+
+// declare function to select Uppercase option
 function includeUpperCase() {
 
 }
+
+// declare function to select Lowercase option
 function includeLowerCase() {
 
 }
