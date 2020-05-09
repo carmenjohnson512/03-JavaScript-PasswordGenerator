ebe2099d6433ae5506822be964c36b4642394e76
Carmen Johnson <carmenjohnson512@gmail.com>
2020-05-09 00:41:20 -0500

    slider value attribution to PW still not working

--- Develop/script.js
+++ Develop/script.js
@@ -1,81 +1,97 @@
 // Assignment Code
-let generateBtn = document.querySelector("#generate");
-let alphNumChar = "abcdegfhijklmnopqrstuvwxyz0123456789".split("");
-console.log("alphNumChar is working!", alphNumChar)
-let specChar = " \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\^\_\`\{\|\}\~".split("");
+var generateBtn = document.querySelector("#generate");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowerCase = "abcdegfhijklmnopqrstuvwxyz".split("");
+// console.log("alphNumChar is working!", alphNumChar)
+var numSpecChar = "0123456789\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\^\_\`\{\|\}\~".split("");
 // console.log("specChar is working!!", specChar)
-let alphNumCharUpper = function () {alphNumChar.toUpperCase()};
+var allChar = "abcdegfhijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\^\_\`\{\|\}\~".split("");
+// console.log("allChar is working!", allChar)
 
-let alphNumCharLower = function () {alphNumChar.toLowerCase()};
- //console.log(alphNumCharLower)
-let alphNumCharChkBx = document.querySelector("#alphNumChar");
-// console.log("AlphNum chk box slector is working!", alphNumCharChkBx)
-let specCharChkBx = document.querySelector("#specChar");
+var numSpecCharChkBx = document.querySelector("#numSpecChar");
 // console.log("SpecChar chk box slector is working!", specCharChkBx)
-let alphNumCharUpperChkBx = document.querySelector("#alphNumCharUpper");
+var upperCaseChkBx = document.querySelector("#upperCaseChar");
 // console.log("Alph Num Upper is working!", alphNumCharUpperChkBx)
-let alphNumCharLowerChkBx = document.querySelector("#alphNumCharLower");
+var lowerCaseCharChkBx = document.querySelector("#lowerCaseChar");
 // console.log("Alph Num Lower is working!", alphNumCharLowerChkBx)
 
 
+var allChoicesArray = [];
+var allCharArray = [];
+var numSpecCharArray = [];
+var upperCaseArray = [];
 
-// if('checkbox for upper is true') {
-//   allChoicesArray.push(allUppers)
-// }
-
-
-// declare function to select Alpha Numeric Character checkbox option
-// function includeAlphNumChar() {
-  
-// }
-
-// // declare function to select Special Character checkbox option
-// function includeSpecChar() {
-
-// }
-
-// // declare function to select Uppercase option
-// function includeUpperCase() {
-
-// }
-
-// // declare function to select Lowercase option
-// function includeLowerCase() {
-
-// }
-
-var allChoicesArray = []
-
+// This function selects the character types to include; can select 1 or 
+//more, but if none, should return empty password field
+//if generate button is pushed
 function decideChoices () {
-  var useAlpha = document.querySelector('#alphNumChar:checked')
-  if (useAlpha != null) {
-    console.log("use alpha!")
-    allChoicesArray = allChoicesArray.concat(alphNumChar)
+  
+  var useUpperCaseChar = document.querySelector('#upperCaseChar:checked');
+  if (useUpperCaseChar != null) {
+    // console.log("use alpha!")
+    upperCaseArray = allChoicesArray.concat(upperCase)
+    console.log("upperCaseArray is working!", upperCaseArray)
+  }
+
+  var useNumSpecChar = document.querySelector('#numSpecChar:checked')
+  if (useNumSpecChar != null) {
+    // console.log("use numSpecChar!", useNumSpecChar)
+    numSpecCharArray = allChoicesArray.concat(numSpecChar)
+    console.log("numSpecCharArray is working!", numSpecCharArray)
+  }
+
+  var useLowerCaseChar = document.querySelector('#lowerCaseChar:checked');
+  if (useLowerCaseChar != null) {
+    // console.log("use alpha!")
+    lowerCaseArray = allChoicesArray.concat(lowerCase)
+    console.log("lowerCaseArray is working!", lowerCaseArray)
+  }
+
+  var useAllChar = document.getElementsByClassName('.charChoice:checked');
+  // console.log("useAllChar is working!", useAllChar)
+  if (useAllChar == true) {
+    allCharArray = allChoicesArray.concat(allChar);
+    console.log("allCharArray is working!", allCharArray)
   }
 }
 
 // // Write password to the #password input
 function writePassword() {
+  // method to set slider value based on slider position
+  var sliderValue = document.querySelector("#slider").value;
+  console.log(sliderValue)
+  var passwordLength = document.querySelector("#password-length");
+  console.log(passwordLength)
+  // // console.log('clicking!!', parseInt(sliderValue))
+  // passwordLength.innerHTML = "Password Length: 8";
   
-  let sliderValue = document.querySelector("#slider").value;
-  console.log('clicking!!', parseInt(sliderValue))
-
+  // // function to set length based on slider position
+//   setPasswordLength = function() {
+//     if(sliderValue >=8) {
+//       passwordLengthValue += sliderValue;
+//     }
+//     else{
+//       passwordLength.value = ""
+//     }
+//   }
+// setPasswordLength();
   // console.log('checkbox checkd ???', document.querySelector('#alphNumCharLower:checked').value
 decideChoices();
 
 
-  if (allChoicesArray.length > 0) {
-    console.log('allChoicesArray', allChoicesArray)
-    var password = ''
-    for(var i =0; i<parseInt(sliderValue); i++){
+  if (allCharArray.length > 0) {
+    // console.log('allChoicesArray', allChoicesArray)
+    var password = "";
+    for(var i = 0; i < parseInt(sliderValue); i++){
       //console.log('looping ?')
       
-      var randomNum = Math.floor(Math.random() * 36)
+      var randomNum = Math.floor(Math.random() * 92);
       //console.log('random num ??', randomNum)
-      password += allChoicesArray[randomNum]
+      password += allCharArray.charAt(randomNum);
+      console.log(password)
     }
 
-    console.log('passowrd filled up ??', password)
+    // console.log('password filled up ??', password)
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
   }
